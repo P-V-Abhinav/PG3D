3719cc25a4fb3173233a69da33cd800ac17ccf4f
Abhinav <rekhakk2004@gmail.com>
2026-08-21 00:58:24 +0530

    viser: (2,3) to (N,2,3)

--- scripts/eval_graspgen_pick.py
+++ scripts/eval_graspgen_pick.py
@@ -627,7 +627,7 @@ def _show_graspgen_viser(
         for j, seg in enumerate(lines):
             server.scene.add_line_segments(
                 f"grasps/{'best' if i == best_idx else i}/{j}",
-                points=seg,
+                points=np.expand_dims(seg, 0),  # (1, 2, 3) for 1 line segment
                 colors=np.tile(color, (2, 1)),
                 line_width=2.0 if i == best_idx else 1.0,
             )
